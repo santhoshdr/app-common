7d9e35c8a20a80a77e36671c623f0288263d7cd4
santhosh <santhoshdesikulkarni@gmail.com>
2019-12-26 17:49:28 +0530

    updated

--- src/main/java/net/drs/common/notifier/NotificationDataConstants.java
+++ src/main/java/net/drs/common/notifier/NotificationDataConstants.java
@@ -4,8 +4,14 @@ public class NotificationDataConstants {
 
    public static final String USER_NAME= "USER_NAME";
    
+   
+   // email
+   public static final String TEMPERORY_ACTIVATION_STRING= "TEMPERORY_ACTIVATION_STRING";
+     
    public static final String EMAIL_ACTIVATION_LINK = "EMAIL_ACTIVATION_LINK";
    
+   
+   // SMS
    public static final String otp = "OTP";
      
     
